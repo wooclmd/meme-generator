275a75c8ae0b7f572808da86a29d6932c8979695
EN CHI YANG <wooclmd@gmail.com>
2023-04-19 13:50:52 +1200

    added async await method

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,6 +18,25 @@ export default function Form() {
       .then((data) => setAllMeme(data.data.memes));
   }, []);
 
+  /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+
+  //   React.useEffect(() => {
+  //     async function getMemes() {
+  //         const res = await fetch("https://api.imgflip.com/get_memes")
+  //         const data = await res.json()
+  //         setAllMemes(data.data.memes)
+  //     }
+  //     getMemes()
+  // }, [])
+
   // get random meme image url from api
   function getMemeImg() {
     const randomNumber = Math.floor(Math.random() * allMeme.length);
